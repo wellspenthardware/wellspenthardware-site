415390879b8caf37801beea4a6b95bb9bde791cc
Octavian Regatun <octavian.regatun@outlook.com>
2020-01-24 20:48:22 +0200

    Fix a mongodb error

--- server/db/connect.js
+++ server/db/connect.js
@@ -3,11 +3,12 @@ const mongoose = require("mongoose");
 mongoose
   .connect(
     `mongodb+srv://wellspenthardwaremod:gibon923@site-pxe1e.mongodb.net/main?retryWrites=true&w=majority`,
-    { useNewUrlParser: true }
+    { useNewUrlParser: true, useUnifiedTopology: true }
   )
-  .catch(e => {
-    console.error("Connection error", e.message);
-  });
+  .then(() => console.log('DB Connected!'))
+.catch(err => {
+console.log(`DB Connection Error: ${err.message}`);
+});
 
 const db = mongoose.connection;
 
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 
-const db = require('./db')
+const db = require('./db/connect')
 
 const app = express()
 const apiPort = 5000
